371745766bff72b05057c2bdd7ab333905d85c13
Brett Meyer <bmeyer0127@gmail.com>
2025-01-02 00:14:07 -0500

    Removed NumberContainer from App.vue. Added very basic click-and-append functionality to buttons

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,58 @@
 <script setup lang="ts">
-  import NumberContainer from './components/NumberContainer.vue'
-  import NumberButton from './components/NumberButton.vue'
+  import { ref } from 'vue';
+  import NumbersContainer from './components/NumbersContainer.vue'
+  import NumberButton from './components/NumberButton.vue';
+  import Screen from './components/Screen.vue'
   import '@material/web/button/filled-button.js'
   import '@material/web/button/outlined-button.js'
   import '@material/web/button/elevated-button.js'
   import '@material/web/checkbox/checkbox.js'
+
+  const numbers = ref([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+  let screenDisplay = ref<string>('');
+
+  const appendNumber = (num: string) => {
+    screenDisplay.value += num;
+  }
 </script>
 
 <template>
-  <header>
-
-  </header>
-
   <main>
-    <NumberContainer />
+    <div class="calc-container">
+      <Screen :display="screenDisplay" class="screen-container"/>
+
+      <div class="numbers-container">
+        <NumberButton v-for="number in numbers" :number="number" :class="'num-class-' + number" @click="appendNumber(number.toString())"/>
+      </div>
+
+    </div>
   </main>
 </template>
+
+<style scoped>
+  .calc-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 50%;
+    height: 100%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .numbers-container {
+    display: grid;
+    grid-template-columns: [col1] 100% [col2] 100% [col3] 100%;
+    grid-template-rows: [row1] 100% [row2] 100% [row3] 100% [row4]100%;
+    width: 50px;
+    height: 50px;
+  }
+
+  .num-class-0 {
+    grid-column-start: 2;
+  }
+  
+  .screen-container {
+    
+  }
+</style>
---
+++ src/components/NumbersContainer.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import NumberButton from './NumberButton.vue';
+  import Screen from './Screen.vue';
+
+  const numbers = ref([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+</script>
+
+<template>
+  <div class="numbers-container">
+    <NumberButton v-for="number in numbers" :number="number" :class="'num-class-' + number" @click="Screen.numA = number"/>
+  </div>
+</template>
+
+<style scoped>
+  .numbers-container {
+    display: grid;
+    grid-template-columns: [col1] 100% [col2] 100% [col3] 100%;
+    grid-template-rows: [row1] 100% [row2] 100% [row3] 100% [row4]100%;
+    width: 50px;
+    height: 50px;
+  }
+
+  .num-class-0 {
+    grid-column-start: 2;
+  }
+</style>
